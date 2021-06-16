02c3524a6ccc4d6c166538896aa32770e217401b
Orlovich Dario <orlovichdario@teksistemi.com>
2021-06-16 09:56:19 +0100

    Add project files.

---
+++ QRER/Program.cs
@@ -0,0 +1,17 @@
+﻿using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+QRCodeGenerator qrGenerator = new();
+if (Uri.IsWellFormedUriString(args[0], UriKind.Absolute))
+{
+    QRCodeData qrCodeData = qrGenerator.CreateQrCode(args[0], QRCodeGenerator.ECCLevel.Q);
+    QRCode qrCode = new QRCode(qrCodeData);
+    Bitmap bmp = qrCode.GetGraphic(20);
+    string path = Path.Combine(Environment.CurrentDirectory, $"QR_{DateTime.Now.Ticks}");
+    bmp.Save(path + ".png", ImageFormat.Png);
+}
+else
+    Console.WriteLine("Specified URL not valid");
