1f05c144ffa2bc08bdc4936034b90da4d2d1d8f3
Orlovich Dario <orlovichdario@teksistemi.com>
2021-10-31 11:10:47 +0000

    clean

--- QRER/Program.cs
+++ QRER/Program.cs
@@ -1,17 +1,14 @@
 ﻿using QRCoder;
 using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
 
 QRCodeGenerator qrGenerator = new();
 if (Uri.IsWellFormedUriString(args[0], UriKind.Absolute))
 {
     QRCodeData qrCodeData = qrGenerator.CreateQrCode(args[0], QRCodeGenerator.ECCLevel.Q);
     QRCode qrCode = new(qrCodeData);
-    Bitmap bmp = qrCode.GetGraphic(20);
-    string path = Path.Combine(Environment.CurrentDirectory, $"QR_{DateTime.Now.Ticks}.png");
-    bmp.Save(path, ImageFormat.Png);
+    System.Drawing.Bitmap bmp = qrCode.GetGraphic(20);
+    string path = System.IO.Path.Combine(Environment.CurrentDirectory, $"QR_{DateTime.Now.Ticks}.png");
+    bmp.Save(path, System.Drawing.Imaging.ImageFormat.Png);
 }
 else
     Console.WriteLine("Specified URL not valid");
